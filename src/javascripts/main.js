539a4d445e64b24223b68f677958c66806b16de5
Lwam Tekie <ltekie87@yahoo.com>
2019-04-20 11:44:52 -0500

    added locations

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,10 @@
 import moviesData from './components/movies/movies';
+import locationsData from './components/locations/locations';
 import '../styles/main.scss';
 
 const init = () => {
   moviesData.initializeMovies();
+  locationsData.initializeLocations();
 };
 
 init();
