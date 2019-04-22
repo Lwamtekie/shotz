6ead74c8c6b616a4ab6a56713e49e23432fbc64b
Lwam Tekie <ltekie87@yahoo.com>
2019-04-22 18:54:56 -0500

    bootstrapcard

--- src/javascripts/components/movies/movies.js
+++ src/javascripts/components/movies/movies.js
@@ -8,7 +8,16 @@ let movies = [];
 const domstringBuilder = () => {
   let domString = '';
   movies.forEach((movie) => {
+    domString += '<div class="card" style="width: 18rem;">';
     domString += `<h3>${movie.name}</h3>`;
+    domString += '<div class="movies-body">';
+    domString += `<h5 class="movie-genre">Movie ${movie.genre}</h5>`;
+    domString += `<h6 class="movie-releaseDate">${movie.releaseDate}</h6>`;
+    domString += `<p class="movie-description">${movie.description}.</p>`;
+    domString += `<h6 class="movie-locations"> ${movie.locations.length} </h5>`;
+    domString += '</div>';
+    domString += '</div>';
+    domString += '</div>';
   });
   util.printToDom('movies', domString);
 };
