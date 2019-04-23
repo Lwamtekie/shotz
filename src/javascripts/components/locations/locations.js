03700d8f3985dc8cc32fcd43bfff14aa30b099df
Lwam Tekie <ltekie87@yahoo.com>
2019-04-22 19:34:50 -0500

    added locations

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -5,10 +5,37 @@ import './locations.scss';
 
 let locations = [];
 
-const domstringBuilder = () => {
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+      selectedClass = '';
+  }
+  return selectedClass;
+};
+
+const domStringBuilder = () => {
   let domString = '';
   locations.forEach((location) => {
-    domString += `<h3>${location.name}</h3>`;
+    domString += `<div id="${location.id}" class="card location col-2">`;
+    domString += `<div class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</div>`;
+    domString += `<img class="card-img-top"> <img src="${location.imageUrl}">`;
+    domString += '<ul class="list-group list-group-flush">';
+    domString += `<li class="list-group-item address"><strong>Address</strong><br> ${location.address}</li>`;
+    domString += '</ul>';
+    domString += '</div>';
   });
   util.printToDom('locations', domString);
 };
@@ -18,9 +45,9 @@ const initializeLocations = () => {
     .then((resp) => {
       const locationsResults = resp.data.locations;
       locations = locationsResults;
-      domstringBuilder();
+      domStringBuilder();
     })
     .catch(err => console.error(err));
 };
 
-export default { initializeLocations };
+export default { initializeLocations, domStringBuilder };
